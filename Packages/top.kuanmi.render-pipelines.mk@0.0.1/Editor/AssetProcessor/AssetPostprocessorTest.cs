1ae610dda5a70f7f63bc270bd88275cab0c6882c
MK <ma_zhengke@sina.com>
2021-08-15 10:52:46 +0800

    AssetPostprocessor

---
+++ Packages/top.kuanmi.render-pipelines.mk@0.0.1/Editor/AssetProcessor/AssetPostprocessorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+namespace KuanMiEditor.Rendering.MKRP
+{
+    public class AssetPostprocessorTest: AssetPostprocessor
+    {
+        public void OnPreprocessMaterialDescription(MaterialDescription description, Material material,
+            AnimationClip[] animations)
+        {
+            Debug.Log("OnPreprocessMaterialDescription");
+            Debug.Log(material);
+        }
+    }
+}
